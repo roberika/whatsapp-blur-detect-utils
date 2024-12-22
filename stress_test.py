5ea406169b46cd9c339b21cb04623718ff0fc7ea
Robert Antonius <robert.antonius23@gmail.com>
2024-12-23 02:12:12 +0700

    Finished testing

--- stress_test.py
+++ stress_test.py
@@ -1,27 +1,41 @@
 import asyncio
 import aiohttp
 import time
-from tqdm import tqdm
+from tqdm.asyncio import tqdm
 
-URL = "whatsapp-blur-detect.vercel.app"
-
-async def post(image, session, progress_bar):
+async def post(session: aiohttp.ClientSession):
+    image = open('image.jpg', 'rb')
     time_start = time.time()
-    async with session.post(URL, files={'image': image}) as response:
-        progress_bar.update(1)
-        await response
+    async with session.post("https://whatsapp-blur-detect.vercel.app/stress-test", data=image) as response:
+        await response.text()
         time_end = time.time()
         return time_end - time_start
+    
+async def bound_post(semaphore, session):
+    async with semaphore:
+        return await post(session)
 
-async def stress_test():
-    with tqdm(total=17040) as progress_bar:
-        with open('image.jpg', 'rb') as image:
-            async with aiohttp.ClientSession() as session:
-                tasks = [asyncio.create_task(coro=post(image, session, progress_bar)) for i in range(1, 10)]
-                times = await asyncio.gather(*tasks, return_exceptions=False)
-                print(f"Tasks        : {len(times)}")
-                print(f"Tasks on-time: {sum([1 if t <= 3 else 0 for t in times])}")
-                print(f"Total time   : {sum(times)}")
-                print(f"Average time : {sum(times) / len(times)}")
+async def stress_test(n):
+    print(f"Running {n} times")
+    semaphore = asyncio.Semaphore(1000)
+    tasks = []
 
-asyncio.run(stress_test())
+    async with aiohttp.ClientSession() as session:
+        for i in range(n):
+            task = asyncio.ensure_future(bound_post(semaphore, session))
+            tasks.append(task)
+            
+        times = await tqdm.gather(*tasks)
+        print(f"Tasks        : {len(times)}")
+        print(f"Tasks on-time: {sum([1 if t <= 3 else 0 for t in times])}")
+        print(f"Total time   : {sum(times)}")
+        print(f"Max time     : {max(times)}")
+        print(f"Average time : {sum(times) / len(times)}")
+
+asyncio.run(stress_test(1))
+asyncio.run(stress_test(2))
+asyncio.run(stress_test(5))
+asyncio.run(stress_test(10))
+asyncio.run(stress_test(20))
+asyncio.run(stress_test(50))
+asyncio.run(stress_test(100))
